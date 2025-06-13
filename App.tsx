e0ed04f1c1a7eb96732d977aa1ddb340a8ffa3cf
Ricardo Rivera <cristianricardoriveramontanez@gmail.com>
2025-06-12 20:26:30 -0500

    implmentacion de fuentes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font'
+import { StatusBar } from 'expo-status-bar'
+import * as SplashScreen from 'expo-splash-screen'
+import { StyleSheet, Text, View } from 'react-native'
+import { useEffect } from 'react'
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    'PlaypenSans-Bold': require('./assets/fonts/PlaypenSans-Bold.ttf'),
+    'PlaypenSans-Regular': require('./assets/fonts/PlaypenSans-Regular.ttf'),
+    'PlaypenSans-Light': require('./assets/fonts/PlaypenSans-Light.ttf'),
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded, error])
+
+  if (!loaded && !error) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hola mundo!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hola mundo estoy con fuente bold!</Text>
+      <Text style={styles.text1}>Hola mundo estoy con fuente  light!</Text>
+      <Text style={styles.text2}>Hola mundo estoy con fuente regular!</Text>
+      <StatusBar style='auto' />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +38,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text: {
+    color: '#000',
+    fontSize: 20,
+    fontFamily: 'PlaypenSans-Bold',
+  },
+  text1: {
+    color: '#000',
+    fontSize: 20,
+    fontFamily: 'PlaypenSans-Light',
+  },
+  text2: {
+    color: '#000',
+    fontSize: 20,
+    fontFamily: 'PlaypenSans-Regular',
+  },
+})
---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require("eslint-config-expo/flat");
+
+module.exports = {
+  extends: ['expo', 'prettier'],
+  ignorePatterns: ['/dist/*'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
